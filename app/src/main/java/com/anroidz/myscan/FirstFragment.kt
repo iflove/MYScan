744334372c1cd12dad3ed5c4dd7b58945b2bee71
iflove <1772885836@qq.com>
2022-01-02 19:50:36 +0800

    Initial commit

---
+++ app/src/main/java/com/anroidz/myscan/FirstFragment.kt
@@ -0,0 +1,49 @@
+package com.anroidz.myscan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.anroidz.myscan.databinding.FragmentFirstBinding
+import com.huawei.hms.scankit.drawable.ScanDrawable
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        val s = ScanDrawable(resources)
+        binding.scanIv.setImageDrawable(s)
+        s.start()
+        s.stop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/huawei/hms/scankit/R.java
@@ -0,0 +1,17 @@
+package com.huawei.hms.scankit;
+
+/**
+ * 作用描述: 组件描述
+ * 组件描述: #基础组件 #组件名 （子组件）
+ * 组件版本: v1
+ * 创建人 rentl
+ * 创建日期 2022/1/2 19:34
+ * 修改日期 2022/1/2 19:34
+ * 版权 mashang
+ */
+//public class R {
+//    public static class drawable {
+//        public final static long scankit_scan_light = com.anroidz.myscan.R.drawable.scankit_scan_light;
+//        public final static long scankit_scan_tail = com.anroidz.myscan.R.drawable.scankit_scan_tail;
+//    }
+//}
